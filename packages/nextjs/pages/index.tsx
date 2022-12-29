715448f2f6f3cad7448962f10103a710f790c848
Bliss Da Sailor <meremezeukah@gmail.com>
2022-12-29 16:23:03 -0600

    batch resolver sample app

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,13 +1,90 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-// import { BugAntIcon, SparklesIcon } from "@heroicons/react/24/outline";
-// import Link from "next/link";
+import { ethers } from "ethers";
 import React, { useState } from "react";
+import { useProvider } from "wagmi";
+
+const ABI = [
+  {
+    stateMutability: "payable",
+    type: "fallback",
+  },
+  {
+    inputs: [
+      {
+        internalType: "contract Registry",
+        name: "registry",
+        type: "address",
+      },
+      {
+        internalType: "bytes32[]",
+        name: "names",
+        type: "bytes32[]",
+      },
+    ],
+    name: "batchResolveWithCustomRegistry",
+    outputs: [
+      {
+        internalType: "address[]",
+        name: "",
+        type: "address[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes32[]",
+        name: "names",
+        type: "bytes32[]",
+      },
+    ],
+    name: "batchResolveWithENSRegistry",
+    outputs: [
+      {
+        internalType: "address[]",
+        name: "",
+        type: "address[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    stateMutability: "payable",
+    type: "receive",
+  },
+];
+
+type ResolvedListItem = {
+  ens: string;
+  address?: `0x${string}` | "Invalid ENS";
+};
 
 const Home: NextPage = () => {
   const [addresses, setAddresses] = useState<string>("");
+  const [resolvedList, setResolvedList] = useState<ResolvedListItem[]>([]);
 
-  console.log({ addresses });
+  const provider = useProvider({ chainId: 1 });
+
+  const handleENSResolve = async () => {
+    const batchResolver = new ethers.Contract("0x4bC4a43Cac594432ED4F48965E5bf5aCA319f350", ABI, provider);
+    const filteredAddresses = addresses.split(/\r?\n/).map(address => address.trim());
+    const hashedAddress = filteredAddresses.map(add => ethers.utils.namehash(add));
+
+    if (hashedAddress.length > 0) {
+      const resolvedAddresses = await batchResolver.batchResolveWithENSRegistry(hashedAddress);
+
+      setResolvedList(
+        resolvedAddresses.map((address: `0x${string}`, i: number) => ({
+          ens: filteredAddresses[i],
+          address: address !== "0x0000000000000000000000000000000000000000" ? address : "Invalid ENS",
+        })),
+      );
+    }
+  };
 
   return (
     <>
@@ -24,6 +101,19 @@ const Home: NextPage = () => {
           placeholder="Enter ENS, one line at a time"
           onChange={e => setAddresses(e.target.value)}
         />
+        <button className="btn btn-primary btn-sm mt-4" onClick={handleENSResolve}>
+          Resolve ENS list
+        </button>
+      </div>
+
+      <div className="flex items-center flex-col flex-grow pt-10">
+        {resolvedList.map(({ ens, address }) => {
+          return (
+            <span key={ens}>
+              {ens} : {address}
+            </span>
+          );
+        })}
       </div>
     </>
   );
